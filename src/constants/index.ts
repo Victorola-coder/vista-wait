9b449fd7fd5805243b6e0e4766a8f35f3a10313c
Victor || VickyJay <victoluolatunji@gmail.com>
2024-08-15 17:05:20 +0100

    constanst and few components

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+export const META_TITLE: string = "Vistapay || Join Waitlist";
+export const META_DESCRIPTION: string = "Be the First to Know!";
+export const META_IMAGE: string = "";
+
+export const META_URL: string = "https://Vistapay.vercel.app/"; //will change to  the real domain later
+export const META_TYPE: string = "website";
+export const META_AUTHOR: string = "Vistapay Dev Team";
+export const META_PUBLISHER: string = "Vistapay Inc";
+export const META_KEYWORDS: string = "Vistapay, Money, Beg, Funds";
+
+// api url
+export const BASE_API_URL = "https://vistapay-api.onrender.com";
