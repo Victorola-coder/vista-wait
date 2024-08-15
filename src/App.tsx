65fc3e5af1908f5e905126c0dad207e71f3fa789
Victor || VickyJay <victoluolatunji@gmail.com>
2024-08-15 17:15:06 +0100

    set up routes && taiwlind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { NotFound, Waitlist } from "./pages";
+import { Route, Routes } from "react-router-dom";
+
 export default function App() {
-  return;
+  return (
+    <Routes>
+      <Route index path="/" element={<Waitlist />} />
+      <Route index path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
   </StrictMode>
 );
